e3d5dd284ff77b9cdffe79522d7dfedbf42f6500
Keshav Kumar <keshavk1002003@gmail.com>
2025-08-06 00:22:33 +0530

    --

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import HomeV from '../assets/HomeV.mp4'
 
 const LandingHero = () => {
   return (
     <section className="relative w-full h-[90vh] overflow-hidden">
       {/* Background Video */}
       <video
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="absolute inset-0 w-full h-full object-cover z-10"
         autoPlay
         loop
         muted
         playsInline
       >
-        <source src="../assets/video_1.mp4" type="video/mp4" />
+        <source src={HomeV} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
       {/* Optional Dark Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-40 z-10"></div>
+      <div className="absolute inset-0 bg-black/40 z-10"></div>
 
       {/* Button */}
       <div className="absolute bottom-10 right-20 z-20">
